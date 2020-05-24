ea3c6ca8b8359f66f0e23287f4c37531dfb1f5fd
SyedSadath <syedsadath101@gmial.com>
2020-05-24 21:26:04 +0530

    %change

--- project/success/javascript.js
+++ project/success/javascript.js
@@ -9,20 +9,51 @@ function range2(){
 	return (Math.random()*10).toFixed(2)
 }
 
+//rang3 : -02.00% to +02.00%
+function range3(){
+	return ((Math.random()*3)+(Math.random()*(-3))).toFixed(3)
+}
+
+
 var stock_names =	[	
 						["bitcoin",9156.20],
 						["ethereum",206.19],
 						["gold",1735],
 						["teslamotoros",816.88],
-						["microsoft",183.51]
+						["microsoft",183.53]
 					];
+
+
+var only_names = ["bitcoin","ethereum","gold","teslamotoros","microsoft"];
 
 let inital_stock_value = new Map(stock_names);
 
-var luckyno = 5555;
-// 1,2
 
-console.log(document.getElementById("stksTable").rows[1].cells.length);
+function current_value(){
+	var row=1;
+	var col=2;
+	for (data of only_names){
+		var value = (inital_stock_value.get(data)+(inital_stock_value.get(data)*range2())).toFixed(2);
+		document.getElementById("stksTable").rows[row].cells[col].innerHTML=value;
+		row++;	
+	}
+}
+
+
+console.log(range3()+"%");
+
+
+
+// for (data of only_names){
+// 	console.log((data));
+// 	row++;	
+// }
+	// keys = inital_stock_value.keys();
+	// console.log(keys[2]);
+
+
+// console.log(document.getElementById("stksTable").rows[1].cells[1].innerHTML="noRiba");
+
 
 
 
